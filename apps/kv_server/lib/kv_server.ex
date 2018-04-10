684555f5d8df8ca2081069132675d03a9f1ec0cd
Garden <kernelgarden@gmail.com>
2018-04-10 21:21:59 +0900

    server 골격 작성

--- apps/kv_server/lib/kv_server.ex
+++ apps/kv_server/lib/kv_server.ex
@@ -1,18 +1,44 @@
+require Logger
+
 defmodule KVServer do
   @moduledoc """
   Documentation for KVServer.
   """
 
-  @doc """
-  Hello world.
+  def accept(port) do
+    {:ok, socket} = :gen_tcp.listen(port,
+                             [:binary, packet: :line, active: false, reuseaddr: true])
+    Logger.info "Accepting connections on port #{port}"
+    loop_acceptor(socket)
+  end
 
-  ## Examples
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    {:ok, pid} = Task.Supervisor.start_child(KVServer.TaskSupervisor, fn -> serve(client) end)
+    :ok = :gen_tcp.controlling_process(client, pid)
+    loop_acceptor(socket)
+  end
 
-      iex> KVServer.hello
-      :world
+  defp serve(socket) do
+    socket
+    |> read_line
+    |> write_line(socket)
 
-  """
-  def hello do
-    :world
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    case :gen_tcp.recv(socket, 0) do
+      {:ok, data} ->
+      Logger.info "Received From Client"
+        data
+      {:error, :closed} ->
+        Logger.info "Closed Session"
+        :error
+    end
+  end
+
+  defp write_line(line, socket) do
+    :gen_tcp.send(socket, line)
   end
 end
--- apps/kv_server/test/kv_server_test.exs
+++ apps/kv_server/test/kv_server_test.exs
@@ -2,7 +2,9 @@ defmodule KVServerTest do
   use ExUnit.Case
   doctest KVServer
 
+  """
   test "greets the world" do
     assert KVServer.hello() == :world
   end
+  """
 end
